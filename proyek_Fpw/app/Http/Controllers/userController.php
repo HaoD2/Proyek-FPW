6833d9ac0e92fab2cca5514282685b6eb2dd16ad
Hao <kevinoctaviushao@gmail.com>
2021-11-28 21:32:50 +0700

    back en untuk login + database

--- proyek_Fpw/app/Http/Controllers/userController.php
+++ proyek_Fpw/app/Http/Controllers/userController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\userModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class userController extends Controller
 {
-    //
+    public function doLogin(Request $req){
+        $email = $req->input('email');
+        $pass = $req->input('pass');
+        $found = false;
+        $dat_user = userModel::all();
+        $credentials = $req->only('username', 'password');
+        if(Auth::attempt($credentials)){
+            foreach ($dat_user as $value) {
+                if($value->Email == $email){
+                    $found = true;
+                    if($value->Password == $pass){
+                        if($value->status == 1){
+                            $users = $value;
+                            $user = Auth::user();
+                            return view('profile');
+                        }else if ($value->status == 0){
+                            return redirect('/')->with("akun_Ban","akun anda terkena ban");
+                        }
+                    }
+                }
+            }
+        }
+        if($found == false){
+            return redirect('/')->with("error_email","Email/Password ada benar");
+        }
+    }
+
+    public function logout(Request $request) {
+        Session::flush();
+        Auth::logout();
+        return Redirect('login');
+    }
 }
